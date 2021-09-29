71847279b966b06af98225ba79c8f7fa3ec87e17
kyo-young <keumky2@gmail.com>
2021-09-29 17:54:26 +0900

    [ADD] mobile page

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -1,16 +1,29 @@
 const subscribe_btns = document.querySelectorAll(".js-subscribe-btn")
 subscribe_btns.forEach((btn) => {
-    btn.addEventListener("click", showSubscribeForm)
+    btn.addEventListener("click", ()=>{showSubscribeForm(".overlay-subscribe")})
 })
 
-function showSubscribeForm() {
-    const overlay = document.querySelector(".overlay");
+function showSubscribeForm(className) {
+    const overlay = document.querySelector(className);
+    console.log(overlay)
     overlay.hidden = false;
 }
-function closeSubscribeForm(){
-    const overlay = document.querySelector(".overlay");
+function closeSubscribeForm(className){
+    const overlay = document.querySelector(className);
     overlay.hidden =true;
 }
 const closeBtn = document.querySelector(".modal-exit")
-closeBtn.addEventListener("click", closeSubscribeForm)
+closeBtn.addEventListener("click",()=>{closeSubscribeForm(".overlay-subscribe")})
 
+const moMenutn = document.querySelector(".mo-menu-btn");
+moMenutn.addEventListener('click',()=>{
+    showSubscribeForm(".mo-menu-overlay")
+})
+
+function makeCloseBtn(btnCloseClassName, componentName){
+    const closeBtn = document.querySelector(btnCloseClassName)
+    closeBtn.addEventListener("click",()=>{closeSubscribeForm(componentName)})
+}
+
+
+makeCloseBtn(".mo-menu-modal-exit",".mo-menu-overlay")
